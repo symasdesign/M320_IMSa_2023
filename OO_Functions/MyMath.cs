35027201382c514a041fec8607c539a83f6c792b
Thomas Kehl <tk@symas.ch>
2023-09-04 08:40:13 +0200

    MyMath added

---
+++ OO_Functions/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_Functions {
+    public static class MyMath {
+
+        public static int Calc_ggT(int a, int b) {
+            return 0;
+        }
+    }
+}
